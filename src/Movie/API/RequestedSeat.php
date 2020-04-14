640550541a0912f6c2b9a9b0fea14a970b7a7e4a
Monika <m852.monika@gmail.com>
2020-04-14 22:02:37 +0200

    Added validation to requested seat using webmozart assert

--- src/Movie/API/RequestedSeat.php
+++ src/Movie/API/RequestedSeat.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace Cinema\Movie\API;
 
+use Webmozart\Assert\Assert;
+
 class RequestedSeat
 {
     /**
@@ -24,9 +26,15 @@ class RequestedSeat
      * @param int $sector
      * @param int $row
      * @param int $seatInRow
+     *
+     * @throws \InvalidArgumentException
      */
     public function __construct(int $sector, int $row, int $seatInRow)
     {
+        Assert::greaterThan($sector, 0, 'The sector number must be greater than 0.');
+        Assert::greaterThan($row, 0, 'The row number must be greater than 0.');
+        Assert::greaterThan($seatInRow, 0, 'The seat in row number must be greater than 0.');
+
         $this->sector = $sector;
         $this->row = $row;
         $this->seatInRow = $seatInRow;
